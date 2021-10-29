bcc8a9a61dea2a0a29eb9e80a87277e73429f992
Tyler Antoni <tyler@dassle.us>
2021-10-29 12:32:18 -0400

    Update Proxzy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "^/api": {
-        target: "https://617c214f6aa7510007f8ff23--dibble-tasktracker-vue.netlify.app/",
+        target: "https://617c214f6aa7510007f8ff23--dibble-tasktracker-vue.netlify.app",
         changeOrigin: true,
         logLevel: "debug",
         pathRewrite: { "^/api": "/" },
